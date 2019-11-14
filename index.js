99325c5cd149f22cf5d743f9c340f28b26a382bd
Justin <nockmss@gmail.com>
2019-11-14 23:56:47 +0000

    Add guessing

--- index.js
+++ index.js
@@ -1,34 +1,117 @@
+/*
+    DOM Navigation
+*/
+
 const SVG_OBJECT_ID = "worldSvg";
-const SVG_OBJECT_TO_ELEMENT = (svgObject) => svgObject.contentDocument.children[0];
+const AREA_SELECT_ID = "areaSelect";
+const APPLY_BUTTON_ID = "applyButton";
+const SVG_OBJECT_TO_ELEMENT = (svgObjectElement) => svgObjectElement.contentDocument.children[0];
 const SVG_ELEMENT_TO_AREAS = (svgElement) => svgElement.children[1].children;
 const SVG_PROP_AREA_NAME = "title";
+
 const style = {
-    areaSelectedColor: 'green',
-    zoomPadding: 50,
+    areaFocusColor: 'gray',
+    areaRightColor: 'green',
+    areaWrongColor: 'red',
+    zoomPadding: 200,
 };
+
+const AREA_GUESS_NONE = null;
+const AREA_GUESS_RIGHT = true;
+const AREA_GUESS_WRONG = false;
+const AREA_NULL = '-- Guess --';
+
+/*
+    State
+*/
 
 let svgElement;
 let areas;
+let selectedArea;
+let selectedGuess;
 
-const svgObject = document.getElementById(SVG_OBJECT_ID);
-svgObject.addEventListener("load",function() {
-    init(svgObject);
+const svgObjectElement = document.getElementById(SVG_OBJECT_ID);
+const areaSelectElement = document.getElementById(AREA_SELECT_ID);
+const applyButtonElement = document.getElementById(APPLY_BUTTON_ID);
+
+const areaGuesses = {};
+
+/*
+    Init
+*/
+
+svgObjectElement.addEventListener("load",function() {
+    init(svgObjectElement);
 }, false);
 
-function init(svgObject) {
-    svgElement = SVG_OBJECT_TO_ELEMENT(svgObject);
-    areas = SVG_ELEMENT_TO_AREAS(svgElement);
-    Array.from(areas).map(initArea);
+function init(svgObjectElement) {
+    svgElement = SVG_OBJECT_TO_ELEMENT(svgObjectElement);
+    initApplyButton();
+    areas = Array.from(SVG_ELEMENT_TO_AREAS(svgElement));
+    areas.map(initArea);
+    areaNames = areas.map((area) => area.getAttribute(SVG_PROP_AREA_NAME))
+    setAreaOptions(areaNames);
+    areaSelectElement.onchange = (ev) => {
+        selectedGuess = ev.currentTarget.value;
+    }
+}
+
+function setAreaOptions(values) {
+    areaSelectElement.innerHTML = [`<option>${AREA_NULL}</option>`].concat(values.map((value) => `<option>${value}</option>`).join(''));
+}
+
+function initApplyButton() {
+    applyButtonElement.onclick = function() {
+        const guess = selectedGuess;
+        const answer = selectedArea.getAttribute(SVG_PROP_AREA_NAME);
+
+        const areaStatus = getAreaStatus(answer);
+        console.log(areaStatus);
+
+        if (areaStatus !== AREA_GUESS_NONE) {
+            console.log('Already guessed! No redos!');
+            return;
+        }
+
+        const correct = guess === answer;
+        const areaGuessStatus = correct ? AREA_GUESS_RIGHT : AREA_GUESS_WRONG;
+        setAreaGuessStatus(selectedArea, areaGuessStatus);
+        console.log(areaGuesses);
+        console.log(selectedGuess);
+        console.log(answer);
+        // console.log(selectedArea);
+        // getAreaStatus
+    }
 }
 
 function initArea(area) {
     area.style.cursor = "pointer";
     area.onclick = function() {
         const areaName = area.getAttribute(SVG_PROP_AREA_NAME);
-        area.style.fill = style.areaSelectedColor;
+        if (getAreaStatus(areaName) === AREA_GUESS_NONE) {
+            area.style.fill = style.areaFocusColor;
+        }
+
         console.log('You clicked:', areaName);
         zoomToBBox(area.getBBox(), style.zoomPadding);
+        selectedArea = area;
     };
+}
+
+function setAreaGuessStatus(area, guessStatus) {
+    const areaName = area.getAttribute(SVG_PROP_AREA_NAME);
+    areaGuesses[areaName] = guessStatus;
+    switch (guessStatus) {
+        case AREA_GUESS_RIGHT: area.style.fill = style.areaRightColor; break;
+        case AREA_GUESS_WRONG: area.style.fill = style.areaWrongColor; break;
+        case AREA_GUESS_NONE: area.style.fill = style.areaFocusColor; break;
+    }
+}
+
+function getAreaStatus(areaName) {
+    const areaGuess = areaGuesses[areaName];
+    if (areaGuess === undefined) { return AREA_GUESS_NONE; }
+    return areaName === areaGuess ? AREA_GUESS_RIGHT : AREA_GUESS_WRONG;
 }
 
 function zoomToBBox(bb, pad) {
